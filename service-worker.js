89aeeae6a639bb5e7d0243d54d1741ea5d3878e3
leeroyjenkins1111 <4xzj45zn2x@privaterelay.appleid.com>
2026-07-31 18:07:45 +0200

    Cache’ować moduł panelu walki

--- service-worker.js
+++ service-worker.js
@@ -10,6 +10,7 @@ const APP_SHELL = [
   './styles/screens.css?v=0.30.1',
   './styles/dice.css?v=0.30.1',
   './styles/atmosphere.css?v=0.30.1',
+  './styles/combat.css?v=0.30.1',
   './scripts/app-config.js?v=0.30.1',
   './scripts/inventory-domain.js?v=0.30.1',
   './scripts/app-core.js?v=0.30.1',
