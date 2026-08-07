92c3d364a667325d533c76b78054b285efab0c4e
leeroyjenkins1111 <4xzj45zn2x@privaterelay.appleid.com>
2026-08-07 19:08:08 +0200

    Remove persistent dice background ring

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cairn-mobile-sheet-v0.30.1-dice-top-projection-v2';
+const CACHE_NAME = 'cairn-mobile-sheet-v0.30.1-dice-top-projection-v3';
 const APP_SHELL = [
   './',
   './index.html',
